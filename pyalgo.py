de7a91459363f234719fdce9e238c41224b81127
nikodiam99 <nikodiam99@gmail.com>
2024-07-06 16:56:38 -0400

    Finished draw info class

--- pyalgo.py
+++ pyalgo.py
@@ -10,6 +10,9 @@ class DrawInformation:
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
 
+    SIDE_PAD = 100
+    TOP_PAD = 150
+
     def __init__(self, width, height, lst): 
         self.width = width
         self.height = height
@@ -19,4 +22,12 @@ class DrawInformation:
         self.set_list(lst)
 
     def self_list(self, lst):
-        pass
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+        #block width determined according to how many items
+        self.block_width = round(self.width - self.SIDE_PAD) / len(lst)
+        #Find out the range of values
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+        
