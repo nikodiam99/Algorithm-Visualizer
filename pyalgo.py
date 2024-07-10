8216a694787831ec585fdf6f56f85cce8525625a
nikodiam99 <nikodiam99@gmail.com>
2024-07-09 22:40:44 -0400

    Create pygame window

--- pyalgo.py
+++ pyalgo.py
@@ -3,31 +3,58 @@ import random
 pygame.init()
 
 class DrawInformation: 
-    BLACK = 0,0,0
-    WHITE = 255, 255, 255
-    GREEN = 0, 255, 0
-    RED = 255, 0, 0
-    GREY = 128, 128, 128
-    BACKGROUND_COLOR = WHITE
+        BLACK = 0,0,0
+        WHITE = 255, 255, 255
+        GREEN = 0, 255, 0
+        RED = 255, 0, 0
+        GREY = 128, 128, 128
+        BACKGROUND_COLOR = WHITE
 
-    SIDE_PAD = 100
-    TOP_PAD = 150
+        SIDE_PAD = 100
+        TOP_PAD = 150
 
-    def __init__(self, width, height, lst): 
-        self.width = width
-        self.height = height
+        def __init__(self, width, height, lst): 
+            self.width = width
+            self.height = height
 
-        self.window = pygame.display.set_mode((width, height)) 
-        pygame.display.set_caption("Sorting Algorithm Visualizer")
-        self.set_list(lst)
+            self.window = pygame.display.set_mode((width, height)) 
+            pygame.display.set_caption("Sorting Algorithm Visualizer")
+            self.set_list(lst)
 
-    def self_list(self, lst):
-        self.lst = lst
-        self.min_val = min(lst)
-        self.max_val = max(lst)
-        #block width determined according to how many items
-        self.block_width = round(self.width - self.SIDE_PAD) / len(lst)
-        #Find out the range of values
-        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
-        self.start_x = self.SIDE_PAD // 2
+        def set_list(self, lst):
+            self.lst = lst
+            self.min_val = min(lst)
+            self.max_val = max(lst)
+            #block width determined according to how many items
+            self.block_width = round(self.width - self.SIDE_PAD) / len(lst)
+            #Find out the range of values
+            self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+            self.start_x = self.SIDE_PAD // 2
+
+def generate_starting_list(n, min_val, max_val):
+        lst = []
+        for _ in range(n):
+            val = random.randint(min_val, max_val)
+            lst.append(val)
+        return lst
+def main():
+        run = True
+        clock = pygame.time.Clock()
+        n = 50
+        min_val = 0
+        max_val = 100
+
+        lst = generate_starting_list(n,min_val,max_val)
+        draw_info = DrawInformation(800,600,lst)
+        
+        while run:
+            clock.tick(60) #60 fps
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+        pygame.quit()
+
+if __name__ == "__main__":
+        main()
         
