207966886904d058320d03d2d38534fc83879f5f
nikodiam99 <nikodiam99@gmail.com>
2024-07-11 23:14:30 -0400

    Draw the columns on the screen

--- pyalgo.py
+++ pyalgo.py
@@ -7,8 +7,13 @@ class DrawInformation:
         WHITE = 255, 255, 255
         GREEN = 0, 255, 0
         RED = 255, 0, 0
-        GREY = 128, 128, 128
         BACKGROUND_COLOR = WHITE
+
+        GRADIENTS = [
+              (128,128,128),
+              (160,160,160),
+              (192,192,192)
+        ]
 
         SIDE_PAD = 100
         TOP_PAD = 150
@@ -30,6 +35,19 @@ class DrawInformation:
             #Find out the range of values
             self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
             self.start_x = self.SIDE_PAD // 2
+def draw(draw_info):
+      draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+      draw_list(draw_info)
+      pygame.display.update()
+def draw_list(draw_info):
+      lst = draw_info.lst
+
+      for i, val in enumerate(lst):
+            x = draw_info.start_x + i * draw_info.block_width
+            y = draw_info.height - (val - draw_info.min_val )* draw_info.block_height 
+
+            color = draw_info.GRADIENTS[i % 3] #every 3 elements reset the gradients
+            pygame.draw.rect(draw_info.window, color, (x,y,draw_info.block_width, draw_info.height))
 
 def generate_starting_list(n, min_val, max_val):
         lst = []
@@ -49,7 +67,7 @@ def main():
         
         while run:
             clock.tick(60) #60 fps
-            pygame.display.update()
+            draw(draw_info)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
