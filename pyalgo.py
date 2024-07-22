8697281992fa34bfb1986f150d66ddd0976f6dbc
nikodiam99 <nikodiam99@gmail.com>
2024-07-22 19:33:42 -0400

    added R keypress reset

--- pyalgo.py
+++ pyalgo.py
@@ -71,6 +71,12 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                if event.type != pygame.KEYDOWN:
+                      continue
+                if event.key == pygame.K_r: #reset the stacks
+                      lst = generate_starting_list(n,min_val,max_val)
+                      draw_info.set_list(lst)
+                      
         pygame.quit()
 
 if __name__ == "__main__":
