0178711cece3fa2050a73e60fd11794e96a809bb
nikodiam99 <nikodiam99@gmail.com>
2024-08-21 16:58:22 -0400

    implemented bubble sort algo

--- pyalgo.py
+++ pyalgo.py
@@ -48,15 +48,24 @@ def draw(draw_info):
       
       draw_list(draw_info)
       pygame.display.update()
-def draw_list(draw_info):
+def draw_list(draw_info, color_positions={}, clear_bg=False):
       lst = draw_info.lst
+      #only clear the list we are drawing
+      if clear_bg:
+            clear_rect = (draw_info.SIDE_PAD//2, draw_info.TOP_PAD, draw_info.width - draw_info.SIDE_PAD, 
+                          draw_info.height - draw_info.TOP_PAD)
+            pygame.draw.rect(draw_info.window, draw_info.BACKGROUND_COLOR, clear_rect)
 
       for i, val in enumerate(lst):
             x = draw_info.start_x + i * draw_info.block_width
             y = draw_info.height - (val - draw_info.min_val )* draw_info.block_height 
 
             color = draw_info.GRADIENTS[i % 3] #every 3 elements reset the gradients
+            if i in color_positions:
+                  color = color_positions[i]
             pygame.draw.rect(draw_info.window, color, (x,y,draw_info.block_width, draw_info.height))
+      if clear_bg:
+            pygame.display.update()
 
 def generate_starting_list(n, min_val, max_val):
         lst = []
@@ -64,6 +73,24 @@ def generate_starting_list(n, min_val, max_val):
             val = random.randint(min_val, max_val)
             lst.append(val)
         return lst
+
+def bubble_sort(draw_info, ascending = True):
+      lst = draw_info.lst
+      for i in range(len(lst) - 1):
+            for j in range(len(lst) - 1 - i):
+                  num1 = lst[j]
+                  num2 = lst[j+1]
+
+                  if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+                        #swap in one line without temp variable in python
+                        lst[j], lst[j+1] = lst[j+1], lst[j]
+                        draw_list(draw_info, {j: draw_info.GREEN, j + 1: draw_info.RED})
+                        #yield pauses but stores current state of function, waits to be called back
+                        #generator
+                        yield True
+      return lst
+                  
+
 def main():
         run = True
         clock = pygame.time.Clock()
